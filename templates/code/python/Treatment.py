15ce4440cabc665ab9dc6254023ee7c22801cac9
frischHWC <frisch@hortonworks.com>
2019-05-06 16:39:32 +0200

    feat(python): add python language possibility

--- templates/code/python/Treatment.py
+++ templates/code/python/Treatment.py
@@ -1,3 +1,5 @@
+from AppConfig import *
+
 {% if "core" is in feature %}
 def treatment(sc):
     """
@@ -10,3 +12,55 @@ def treatment(sc):
     rdd.foreach(lambda x: print("x * 2 = ", x*2))
     {% endif %}
 
+{% if "sql" is in feature %}
+def treatment_sql(spark):
+    """
+    SQL treatment on Spark
+    :param spark:
+    :return:
+    """
+    df_init = spark.read \
+        .load(hdfs + hdfs_home_dir + "les-arbres.csv",
+              format="csv", sep=";", inferSchema="true", header="true")
+
+    df_init.select("GENRE").show()
+    {% endif %}
+
+{% if "structured_streaming" is in feature %}
+def treatment_structured_streaming(spark):
+    """
+    Structured Streaming on Spark
+    :param spark:
+    :return:
+    """
+    df_streamed = spark.readStream \
+        .parquet(hdfs + hdfs_home_dir + "les-arbres.parquet")
+
+    df_streamed.writeStream.format("parquet") \
+        .option("checkpointLocation", hdfs + hdfs_home_dir + "checkpoints/") \
+        .option("path", hdfs + hdfs_home_dir + "les-arbres2.parquet") \
+        .start()
+
+    spark.streams.awaitAnyTermination()
+    {% endif %}
+
+{% if "streaming" is in feature %}
+def treatment_streaming(ssc):
+    """
+    Streaming on Spark
+    :param ssc:
+    :return:
+    """
+    streamfile = ssc.textFileStream(hdfs + hdfs_home_dir + "streaming/")
+
+    def apply_to_each_row(row):
+        print(row)
+
+    def apply_to_each_rdd(rdd):
+        rdd.foreach(apply_to_each_row)
+
+    streamfile.foreachRDD(apply_to_each_rdd)
+
+    ssc.start()
+    ssc.awaitTermination()
+    {% endif %}
